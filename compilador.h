8fc4d3dc4a2b4cd8e74623ff7253330c7a48783d
Lucas Block <lucas.blockv@gmail.com>
2021-06-07 21:27:00 -0300

    adicionado todos simbolos enum

--- compilador.h
+++ compilador.h
@@ -18,6 +18,14 @@ typedef enum simbolos {
   simb_identificador, simb_numero,
   simb_ponto, simb_virgula, simb_ponto_e_virgula, simb_dois_pontos,
   simb_atribuicao, simb_abre_parenteses, simb_fecha_parenteses,
+  
+  simb_label, simb_type, simb_array, simb_of, simb_var, simb_procedure,
+  simb_function, simb_goto, simb_if, simb_then, simb_else, simb_while,
+  simb_do, simb_mais, simb_menos, simb_multiplicacao, simb_or, simb_div,
+  simb_and, simb_not, simb_abre_colchetes, simb_fecha_colchetes, simb_abre_chaves,
+  simb_fecha_chaves, simb_igual, simb_maior, simb_menor, simb_diferente, simb_maior_igual,
+  simb_menor_igual, simb_read, simb_write, simb_false, simb_true
+
 } simbolos;
 
 
